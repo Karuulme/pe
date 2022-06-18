ec692349ee348204e159fefe34ee382a428a4842
Muhammet Karul <64870233+Karuulme@users.noreply.github.com>
2022-06-18 18:04:13 +0300

    line line read

--- Strings/Strings.cpp
+++ Strings/Strings.cpp
@@ -1,45 +1,48 @@
 ﻿#include <iostream>
 #include <string>
 #include <fstream>
-#include <string>
 
 using namespace std;
 
 void FileReadAndWrite(ifstream* FRead, ofstream* FWrite) {
-    char buf[50];
-    FRead->read(buf, sizeof(buf));
-    if (FRead) {
-        while (FRead) {
-            if (FRead->read(buf, sizeof(buf)))
-            {
-                int LineCont = 0, Linelengh = 0;
-                string Sentence="";
-                for (int i = 0; i < sizeof(buf); i++) {
-                    if (32 <= (int)buf[i] && (int)buf[i] <= 126) {
-                        Sentence += buf[i];
-                        Linelengh++;
-                        LineCont = 1;
-                    }
-                    else {
-                        if (LineCont == 1)
+    string buf;
+    //FRead->getline(&FRead, buf);
+    if (FRead->is_open()) {
+        while (getline(*FRead, buf)) {
+            for (int i = 0; i < buf.length(); i++) {
+                if (32 <= (int)buf[i] && (int)buf[i] <= 126) {
+                    cout << buf[i];
+                }
+            }
+            cout << endl;
+        }
+        
+            /*
+            int LineCont = 0, Linelengh = 0;
+            string Sentence="";
+            for (int i = 0; i < sizeof(buf); i++) {
+                if (32 <= (int)buf[i] && (int)buf[i] <= 126) {
+                    Sentence += buf[i];
+                    Linelengh++;
+                    LineCont = 1;
+                   
+                }
+                else {
+                    if (LineCont == 1)
+                    {
+                        LineCont = 0;
+                        if (Sentence.length() >1)
                         {
-                            LineCont = 0;
-                            if (Sentence.length() > 2)
-                            {
-                                cout << Sentence;
-                                cout << endl;
-
-                            }
-                            Linelengh = 0;
-                            Sentence = "";
-                            //  FWrite->write("\0", sizeof(buf[i]));
-                            
+                            cout << Sentence;
+                            cout << endl;
                         }
+                        Linelengh = 0;
+                        Sentence = "";
+                        //  FWrite->write("\0", sizeof(buf[i]));
+                        
                     }
                 }
- 
-            }
-        }
+            }*/
     }
 }
 int main()
@@ -52,5 +55,6 @@ int main()
     fileRead.close();
     FileWrite.close();
     return 0;
+     
 }
 
