2e30b4d018b5c7c4c1f057967aa98909de25bb2e
Muhammet Karul <64870233+Karuulme@users.noreply.github.com>
2022-06-18 17:37:19 +0300

    İleriler

--- Strings/Strings.cpp
+++ Strings/Strings.cpp
@@ -14,7 +14,6 @@ void FileReadAndWrite(ifstream* FRead, ofstream* FWrite) {
             {
                 int LineCont = 0, Linelengh = 0;
                 string Sentence="";
-
                 for (int i = 0; i < sizeof(buf); i++) {
                     if (32 <= (int)buf[i] && (int)buf[i] <= 126) {
                         Sentence += buf[i];
@@ -25,9 +24,16 @@ void FileReadAndWrite(ifstream* FRead, ofstream* FWrite) {
                         if (LineCont == 1)
                         {
                             LineCont = 0;
-                            cout << Sentence;
+                            if (Sentence.length() > 2)
+                            {
+                                cout << Sentence;
+                                cout << endl;
+
+                            }
+                            Linelengh = 0;
+                            Sentence = "";
                             //  FWrite->write("\0", sizeof(buf[i]));
-                            cout << endl;
+                            
                         }
                     }
                 }
@@ -40,7 +46,9 @@ int main()
 {
     ifstream fileRead("C:/Users/SanalMak/Desktop/Analiz edilenler/analiz1/71257279-042b-371d-a1d3-fbf8d2fadffa.exe", ios::in | ios::binary);// Okunacak Dosya
     ofstream FileWrite("C:/Users/SanalMak/Desktop/Analiz edilenler/analiz1/deneme.txt"); // Yazılacak dosya
+
     FileReadAndWrite(&fileRead, &FileWrite);
+
     fileRead.close();
     FileWrite.close();
     return 0;
