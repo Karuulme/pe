6168b5fb48d1ecd87d98621d73ab73e0bad9f254
Muhammet Karul <64870233+Karuulme@users.noreply.github.com>
2022-06-18 18:52:10 +0300

    Okunabilir Çıktı

--- Strings/Strings.cpp
+++ Strings/Strings.cpp
@@ -1,59 +1,32 @@
 ﻿#include <iostream>
 #include <string>
 #include <fstream>
-
 using namespace std;
 
-void FileReadAndWrite(ifstream* FRead, ofstream* FWrite) {
-    string buf;
-    //FRead->getline(&FRead, buf);
-    if (FRead->is_open()) {
-        while (getline(*FRead, buf)) {
+void FileReadAndWrite(string* FReadName,string *FWriteName) {
+    ifstream FRead(*FReadName, ios::in | ios::binary);// Read File
+    ofstream FWrite(*FWriteName, ios::out | ios::app);// Write File
+    if (FRead.is_open()) {
+        string buf;
+        while (getline(FRead, buf)) {
             for (int i = 0; i < buf.length(); i++) {
                 if (32 <= (int)buf[i] && (int)buf[i] <= 126) {
-                    cout << buf[i];
+                    FWrite<< buf[i];
                 }
             }
+            FWrite<<endl;
             cout << endl;
         }
-        
-            /*
-            int LineCont = 0, Linelengh = 0;
-            string Sentence="";
-            for (int i = 0; i < sizeof(buf); i++) {
-                if (32 <= (int)buf[i] && (int)buf[i] <= 126) {
-                    Sentence += buf[i];
-                    Linelengh++;
-                    LineCont = 1;
-                   
-                }
-                else {
-                    if (LineCont == 1)
-                    {
-                        LineCont = 0;
-                        if (Sentence.length() >1)
-                        {
-                            cout << Sentence;
-                            cout << endl;
-                        }
-                        Linelengh = 0;
-                        Sentence = "";
-                        //  FWrite->write("\0", sizeof(buf[i]));
-                        
-                    }
-                }
-            }*/
     }
+    FRead.close();
+    FWrite.close();
 }
 int main()
 {
-    ifstream fileRead("C:/Users/SanalMak/Desktop/Analiz edilenler/analiz1/71257279-042b-371d-a1d3-fbf8d2fadffa.exe", ios::in | ios::binary);// Okunacak Dosya
-    ofstream FileWrite("C:/Users/SanalMak/Desktop/Analiz edilenler/analiz1/deneme.txt"); // Yazılacak dosya
+    string FileRead = "C:/Users/SanalMak/Desktop/Analiz edilenler/analiz1/71257279-042b-371d-a1d3-fbf8d2fadffa.exe";
+    string FileWrite = "C:/Users/SanalMak/Desktop/Analiz edilenler/analiz1/deneme.txt";
+    FileReadAndWrite(&FileRead,&FileWrite);
 
-    FileReadAndWrite(&fileRead, &FileWrite);
-
-    fileRead.close();
-    FileWrite.close();
     return 0;
      
 }
