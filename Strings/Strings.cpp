be5c77189819953d71a08fdb89c821d82b1a161c
Muhammet Karul <64870233+Karuulme@users.noreply.github.com>
2022-06-18 16:56:46 +0300

    Proje dosyası ekle.

---
+++ Strings/Strings.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void FileReadAndWrite(ifstream* FRead, ofstream* FWrite) {
+    char buf[50];
+    FRead->read(buf, sizeof(buf));
+    if (FRead) {
+        while (FRead) {
+            if (FRead->read(buf, sizeof(buf)))
+            {
+                int LineCont = 0, Linelengh = 0;
+                string Sentence="";
+
+                for (int i = 0; i < sizeof(buf); i++) {
+                    if (32 <= (int)buf[i] && (int)buf[i] <= 126) {
+                        Sentence += buf[i];
+                        Linelengh++;
+                        LineCont = 1;
+                    }
+                    else {
+                        if (LineCont == 1)
+                        {
+                            LineCont = 0;
+                            cout << Sentence;
+                            //  FWrite->write("\0", sizeof(buf[i]));
+                            cout << endl;
+                        }
+                    }
+                }
+ 
+            }
+        }
+    }
+}
+int main()
+{
+    ifstream fileRead("C:/Users/SanalMak/Desktop/Analiz edilenler/analiz1/71257279-042b-371d-a1d3-fbf8d2fadffa.exe", ios::in | ios::binary);// Okunacak Dosya
+    ofstream FileWrite("C:/Users/SanalMak/Desktop/Analiz edilenler/analiz1/deneme.txt"); // Yazılacak dosya
+    FileReadAndWrite(&fileRead, &FileWrite);
+    fileRead.close();
+    FileWrite.close();
+    return 0;
+}
+
