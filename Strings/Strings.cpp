92feaa215230e1345ff9e31641fa1a5c81b5a5ee
Muhammet Karul <64870233+Karuulme@users.noreply.github.com>
2022-06-20 11:14:15 +0300

    FileExtension, WindowsFunctionsAnddllFiles and Links functions

--- Strings/Strings.cpp
+++ Strings/Strings.cpp
@@ -4,20 +4,98 @@
 #include <string>
 using namespace std;
 
-string FileAddresses[2];// 0. Index Read File , 1. Index Read File
 
-DWORD WINAPI FileThread(LPVOID lpParam) {
-	if (!FileAddresses[0].empty() && !FileAddresses[1].empty()) {
-		ifstream FRead(FileAddresses[0], ios::in | ios::binary);// Read File
-		ofstream FWrite(FileAddresses[1], ios::out | ios::app);// Write File
+
+template<class T>
+class List
+{
+public:
+    int Remove(unsigned int index) {
+        try {
+            _Length--;
+            T** NewStartPoint = StartPoint;
+            StartPoint = new T * [_Length];
+            for (int i = 0, j = 0; i < _Length + 1; i++)
+            {
+                if (index != i) {
+                    StartPoint[j] = NewStartPoint[i];
+                    j++;
+                }
+
+            }
+            delete(NewStartPoint);
+            return 1;
+        }
+        catch (int myNum) {
+            return 0;
+        }
+    }
+    int GetIndex(unsigned int i) {
+        return (i >= _Length) ? 0 : *StartPoint[i];
+    }
+    void Read() {
+        for (int i = 0; i < _Length; i++) {
+            cout << *StartPoint[i] << endl;
+        }
+    }
+    int Length() {
+        return _Length;
+    }
+    void Add(T value)
+    {
+        MemoryPointCreate(&value);
+    }
+    int* GetAll() {
+        int GetAllList[_Length];
+        for (int i = 0; i < _Length; i++) {
+            GetAllList[i] = *StartPoint[i];
+        }
+        return GetAllList;
+    }
+    ~List() {
+        delete _Length;
+        delete StartPoint;
+    }
+private:
+    int _Length = 0;
+    T** StartPoint;
+
+    void MemoryPointCreate(T* value) {
+        T* NPoint = new T[1];
+        *NPoint = *value;
+        MemoryExpansion(NPoint);
+    }
+    void MemoryExpansion(T* NValue) {
+        if (_Length > 0) {
+            _Length++;
+            T** NewStartPoint = StartPoint;
+            StartPoint = new T * [_Length];
+            for (int i = 0; i < _Length - 1; i++)
+            {
+                StartPoint[i] = NewStartPoint[i];
+            }
+            StartPoint[_Length - 1] = NValue;
+            delete(NewStartPoint);
+        }
+        else {
+            _Length++;
+            StartPoint = new T * [_Length];
+            StartPoint[0] = NValue;
+        }
+    }
+};
+
+int FileAnalysis(string* AddressRead, List<string>* FileLines) {
+    if (!(*AddressRead).empty()) {
+        ifstream FRead(*AddressRead, ios::in | ios::binary);// Read File
         string buf;
-       // FRead.read(buf, sizeof(buf));
-        if (FRead && FWrite) {
-            while (getline(FRead,buf)) {
+
+        if (FRead) {
+            while (getline(FRead, buf)) {
                 int LineCont = 0;
                 string Sentence = "";
-                for (int i = 0; i <buf.length(); i++) {
-                    if (32 <= (int)buf[i] && (int)buf[i] <= 126) {
+                for (int i = 0; i < buf.length(); i++) {
+                    if (32 <= (int)buf[i] && (int)buf[i] <= 126 || buf[i] == ' ' || buf[i] == '*') {
                         Sentence += buf[i];
                         LineCont = 1;
                     }
@@ -25,37 +103,30 @@ DWORD WINAPI FileThread(LPVOID lpParam) {
                         if (LineCont == 1)
                         {
                             LineCont = 0;
-                            if (Sentence.length() > 1)
-                            {
-                                FWrite << Sentence;
-                                FWrite << endl;
-
-                            }
+                            FileLines->Add(Sentence);
                             Sentence = "";
                         }
                     }
                 }
             }
         }
-		FRead.close();
-		FWrite.close();
-	}
-	return 0;
+        FRead.close();
+        return 1;
+    }
+    return 0;
 }
 int main()
 {
-	HANDLE hThread;
-	DWORD ThreadID;
+    string FileAddressesRead;
+    // string FileAddressesWrite;
+    List<string> FileLines;
+    List<string> FileExtension;
+    List<string> WindowsFunctionsAnddllFiles;
+    List<string> Links;
 
-    FileAddresses[0] = "C:/Users/SanalMak/Desktop/Analiz edilenler/analiz1/71257279-042b-371d-a1d3-fbf8d2fadffa.exe";
-    FileAddresses[1] = "C:/Users/SanalMak/Desktop/Analiz edilenler/analiz1/deneme.txt";
+    FileAddressesRead = L'C:/Users/SanalMak/Desktop/Analiz edilenler/analiz1/71257279-042b-371d-a1d3-fbf8d2fadffa.exe';
+    //  FileAddressesWrite = L'C: / Users / SanalMak / Desktop / Analiz edilenler / analiz1 / deneme.txt';
 
-	hThread = CreateThread(NULL, 0, FileThread, (void*)&FileAddresses, 0, &ThreadID);
-	if (hThread) {
-		cout << " Başarıyla oluşturuldu ";
-		cout << endl;
-		cout << ThreadID;
-	}
-	CloseHandle(hThread);
+    FileAnalysis(&FileAddressesRead, &FileLines);
+
 }
-
