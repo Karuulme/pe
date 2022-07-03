c5501258b7eadd133cddce85f922b18bd91f3218
Muhammet Karul <64870233+Karuulme@users.noreply.github.com>
2022-07-03 10:45:47 +0300

    simplification

--- Strings/Strings.cpp
+++ Strings/Strings.cpp
@@ -1,132 +1,62 @@
 ﻿#include <iostream>
-#include <windows.h>
 #include <fstream>
 #include <string>
+#include <stdio.h> 
+#include <stdlib.h> 
 using namespace std;
+string HexBase(char str) {
+    int TenBase = (int)str;
+    string Basees[16] = { "0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F" };
+    int bolum = (int)(TenBase / 16);
+    string kalan = Basees[TenBase % 16];
+    string donusDegeri = to_string(bolum) + kalan;
+    return donusDegeri;
+}
+int BinaryCharacter(char* arr, char Binary) {
 
-
-
-template<class T>
-class List
-{
-public:
-    int Remove(unsigned int index) {
-        try {
-            _Length--;
-            T** NewStartPoint = StartPoint;
-            StartPoint = new T * [_Length];
-            for (int i = 0, j = 0; i < _Length + 1; i++)
-            {
-                if (index != i) {
-                    StartPoint[j] = NewStartPoint[i];
-                    j++;
-                }
-
-            }
-            delete(NewStartPoint);
-            return 1;
-        }
-        catch (int myNum) {
-            return 0;
-        }
+    for (int i = 0; i < 256; i++) {
+        if (arr[i] == Binary)return i;
     }
-    int GetIndex(unsigned int i) {
-        return (i >= _Length) ? 0 : *StartPoint[i];
-    }
-    void Read() {
-        for (int i = 0; i < _Length; i++) {
-            cout << *StartPoint[i] << endl;
-        }
-    }
-    int Length() {
-        return _Length;
-    }
-    void Add(T value)
-    {
-        MemoryPointCreate(&value);
-    }
-    int* GetAll() {
-        int GetAllList[_Length];
-        for (int i = 0; i < _Length; i++) {
-            GetAllList[i] = *StartPoint[i];
-        }
-        return GetAllList;
-    }
-    ~List() {
-        delete _Length;
-        delete StartPoint;
-    }
-private:
-    int _Length = 0;
-    T** StartPoint;
-
-    void MemoryPointCreate(T* value) {
-        T* NPoint = new T[1];
-        *NPoint = *value;
-        MemoryExpansion(NPoint);
-    }
-    void MemoryExpansion(T* NValue) {
-        if (_Length > 0) {
-            _Length++;
-            T** NewStartPoint = StartPoint;
-            StartPoint = new T * [_Length];
-            for (int i = 0; i < _Length - 1; i++)
-            {
-                StartPoint[i] = NewStartPoint[i];
-            }
-            StartPoint[_Length - 1] = NValue;
-            delete(NewStartPoint);
-        }
-        else {
-            _Length++;
-            StartPoint = new T * [_Length];
-            StartPoint[0] = NValue;
-        }
-    }
-};
-
-int FileAnalysis(string* AddressRead, List<string>* FileLines) {
-    if (!(*AddressRead).empty()) {
-        ifstream FRead(*AddressRead, ios::in | ios::binary);// Read File
-        string buf;
-
-        if (FRead) {
-            while (getline(FRead, buf)) {
-                int LineCont = 0;
-                string Sentence = "";
-                for (int i = 0; i < buf.length(); i++) {
-                    if (32 <= (int)buf[i] && (int)buf[i] <= 126 || buf[i] == ' ' || buf[i] == '*') {
-                        Sentence += buf[i];
-                        LineCont = 1;
-                    }
-                    else {
-                        if (LineCont == 1)
-                        {
-                            LineCont = 0;
-                            FileLines->Add(Sentence);
-                            Sentence = "";
-                        }
-                    }
-                }
-            }
-        }
-        FRead.close();
-        return 1;
-    }
-    return 0;
+    return 32;
 }
 int main()
 {
-    string FileAddressesRead;
-    // string FileAddressesWrite;
-    List<string> FileLines;
-    List<string> FileExtension;
-    List<string> WindowsFunctionsAnddllFiles;
-    List<string> Links;
+    char Characteres[256];
+    for (int i = 0; i < 256; i++) {
+        Characteres[i] = (char)i;
+    }
+    //setlocale(LC_ALL, "Turkish");
+    ifstream FileRead("C:/Users/SanalMak/Desktop/malware kurs içeriği/Örnek Zararlılar/virusler/Practical Malware Analysis Labs/BinaryCollection/Chapter_10L/Lab10-01.exe", ios::binary | ios::in);
+    ofstream FileWeite("C:/Users/SanalMak/Desktop/malware kurs içeriği/Örnek Zararlılar/virusler/Practical Malware Analysis Labs/BinaryCollection/Chapter_10L/Lab10-01.txt");
+    if (FileRead.is_open()) {
+        string buf = "";
+        while (getline(FileRead, buf))
+        {
+            string bufBinary = "";
+            char Line[8];
+            for (int i = 0, j = 0; i < buf.length(); i++, j++) {
+                if (31 < (int)buf[i] && (int)buf[i] != 127) {
+                    bufBinary += " " + HexBase((Characteres, (char)buf[i])) + " ";
+                    Line[j] = buf[i];
+                }
+                else {
+                    bufBinary += "_";
+                    Line[j] = '.';
+                }
+                if (j == 7) {
+                    FileWeite << bufBinary << " ---------- ";
+                    cout << bufBinary << " ---------- ";
+                    for (int j = 0; j < 8; j++) {
+                        FileWeite << Line[j];
+                        cout << Line[j];
+                    }
+                    j = 0;
+                    FileWeite << endl;
+                    cout << endl;
+                    bufBinary = "";
+                }
 
-    FileAddressesRead = L'C:/Users/SanalMak/Desktop/Analiz edilenler/analiz1/71257279-042b-371d-a1d3-fbf8d2fadffa.exe';
-    //  FileAddressesWrite = L'C: / Users / SanalMak / Desktop / Analiz edilenler / analiz1 / deneme.txt';
-
-    FileAnalysis(&FileAddressesRead, &FileLines);
-
+            }
+        }
+    }
 }
