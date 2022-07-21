cccf11d779acfa5e4303dfedea9eca1deb25702d
Muhammet Karul <64870233+Karuulme@users.noreply.github.com>
2022-07-21 20:27:03 +0300

    Dos and Pe file printing

--- Strings/Pe.cpp
+++ Strings/Pe.cpp
@@ -6,63 +6,79 @@ using namespace std;
 
 int main()
 {
-    IMAGE_NT_HEADERS    ntHeaders;
+
+
+    // TITLE TRANSITIONS  
+    //     Gerekmedikçe kullanmıyoruz tabiki, önce araştırın, daha hızlı öğrenirsiniz. takıldığınız noktada bakın :)
+    //     ->
+    //     MAGE_DOS_HEADER pidh = (PIMAGE_DOS_HEADER)buffer;
+    //     PIMAGE_NT_HEADERS pinh = (PIMAGE_NT_HEADERS)((BYTE*)pidh + pidh->e_lfanew);
+    //     EFILE_HEADER pifh = (PIMAGE_FILE_HEADER)&pinh->FileHeader;
+    //     EOPTIONAL_HEADER pioh = (PIMAGE_OPTIONAL_HEADER)&pinh->OptionalHeader;
+    //	   ESECTION_HEADER pish = (PIMAGE_SECTION_HEADER)((BYTE*)pinh + sizeof(IMAGE_NT_HEADERS) + (pifh->NumberOfSections - 1) * sizeof(IMAGE_SECTION_HEADER));
+
+
+    // MAGE_DOS_HEADER pidh   -> PIMAGE_NT_HEADERS  ->   EFILE_HEADER   ->    EOPTIONAL_HEADER    ->   ESECTION_HEADER
+
+
+    PIMAGE_NT_HEADERS    ntHeaders;
     //            ->
     //            DWORD                   Signature;
     //            IMAGE_FILE_HEADER       FileHeader;
     //            IMAGE_OPTIONAL_HEADER32 OptionalHeader;
-    IMAGE_OPTIONAL_HEADER   optinalHeader;
-    //                 ->
-    //                 WORD                 Magic;
-    //                 BYTE                 MajorLinkerVersion;
-    //                 BYTE                 MinorLinkerVersion;
-    //                 DWORD                SizeOfCode;
-    //                 DWORD                SizeOfInitializedData;
-    //                 DWORD                SizeOfUninitializedData;
-    //                 DWORD                AddressOfEntryPoint;
-    //                 DWORD                BaseOfCode;
-    //                 DWORD                BaseOfData;
-    //                 DWORD                ImageBase;
-    //                 DWORD                SectionAlignment;
-    //                 DWORD                FileAlignment;
-    //                 WORD                 MajorOperatingSystemVersion;
-    //                 WORD                 MinorOperatingSystemVersion;
-    //                 WORD                 MajorImageVersion;
-    //                 WORD                 MinorImageVersion;
-    //                 WORD                 MajorSubsystemVersion;
-    //                 WORD                 MinorSubsystemVersion;
-    //                 DWORD                Win32VersionValue;
-    //                 DWORD                SizeOfImage;
-    //                 DWORD                SizeOfHeaders;
-    //                 DWORD                CheckSum;
-    //                 WORD                 Subsystem;
-    //                 WORD                 DllCharacteristics;
-    //                 DWORD                SizeOfStackReserve;
-    //                 DWORD                SizeOfStackCommit;
-    //                 DWORD                SizeOfHeapReserve;
-    //                 DWORD                SizeOfHeapCommit;
-    //                 DWORD                LoaderFlags;
-    //                 DWORD                NumberOfRvaAndSizes;
-    //                 IMAGE_DATA_DIRECTORY DataDirectory[IMAGE_NUMBEROF_DIRECTORY_ENTRIES];
-    IMAGE_DATA_DIRECTORY    dataDirectory;
-    //                -> 
-    //                DWORD VirtualAddress;
-    //                DWORD Size;
-    IMAGE_SECTION_HEADER    sectionHeader;
-    //                ->
-    //                BYTE  Name[IMAGE_SIZEOF_SHORT_NAME];
-    //               union {
-    //                    DWORD PhysicalAddress;
-    //                    DWORD VirtualSize;
-    //                } Misc;
-    //                DWORD VirtualAddress;
-    //                DWORD SizeOfRawData;
-    //                DWORD PointerToRawData;
-    //                DWORD PointerToRelocations;
-    //                DWORD PointerToLinenumbers;
-    //                WORD  NumberOfRelocations;
-    //                WORD  NumberOfLinenumbers;
-    //                DWORD Characteristics;
+    PIMAGE_OPTIONAL_HEADER   optinalHeader;
+    //            ->
+    //            WORD                 Magic;
+    //            BYTE                 MajorLinkerVersion;
+    //            BYTE                 MinorLinkerVersion;
+    //            DWORD                SizeOfCode;
+    //            DWORD                SizeOfInitializedData;
+    //            DWORD                SizeOfUninitializedData;
+    //            DWORD                AddressOfEntryPoint;
+    //            DWORD                BaseOfCode;
+    //            DWORD                BaseOfData;
+    //            DWORD                ImageBase;
+    //            DWORD                SectionAlignment;
+    //            DWORD                FileAlignment;
+    //            WORD                 MajorOperatingSystemVersion;
+    //            WORD                 MinorOperatingSystemVersion;
+    //            WORD                 MajorImageVersion;
+    //            WORD                 MinorImageVersion;
+    //            WORD                 MajorSubsystemVersion;
+    //            WORD                 MinorSubsystemVersion;
+    //            DWORD                Win32VersionValue;
+    //            DWORD                SizeOfImage;
+    //            DWORD                SizeOfHeaders;
+    //            DWORD                CheckSum;
+    //            WORD                 Subsystem;
+    //            WORD                 DllCharacteristics;
+    //            DWORD                SizeOfStackReserve;
+    //            DWORD                SizeOfStackCommit;
+    //            DWORD                SizeOfHeapReserve;
+    //            DWORD                SizeOfHeapCommit;
+    //            DWORD                LoaderFlags;
+    //            DWORD                NumberOfRvaAndSizes;
+    //            IMAGE_DATA_DIRECTORY DataDirectory[IMAGE_NUMBEROF_DIRECTORY_ENTRIES];
+    PIMAGE_DATA_DIRECTORY    dataDirectory;
+    //            -> 
+    //            DWORD VirtualAddress;
+    //            DWORD Size;
+    PIMAGE_SECTION_HEADER    sectionHeader;
+    //            ->
+    //            BYTE  Name[IMAGE_SIZEOF_SHORT_NAME];
+    //            union 
+    //		      {
+    //              DWORD PhysicalAddress;
+    //            	DWORD VirtualSize;
+    //            } Misc;
+    //            DWORD VirtualAddress;
+    //            DWORD SizeOfRawData;
+    //            DWORD PointerToRawData;
+    //            DWORD PointerToRelocations;
+    //            DWORD PointerToLinenumbers;
+    //            WORD  NumberOfRelocations;
+    //            WORD  NumberOfLinenumbers;
+    //            DWORD Characteristics;
     PIMAGE_DOS_HEADER dosHeader;   //   DOS başlığı
     //            ->
     //            WORD   e_magic;                     // Magic number
@@ -84,15 +100,21 @@ int main()
     //            WORD   e_oeminfo;                   // OEM information; e_oemid specific
     //            WORD   e_res2[10];                  // Reserved words
     //            LONG   e_lfanew;                    // File address of new exe header
-    IMAGE_FILE_HEADER fileHeader;
-    //             ->
+    PIMAGE_FILE_HEADER fileHeader;
+    //            ->
+    //            WORD  Machine;
+    //            WORD  NumberOfSections;
+    //            DWORD TimeDateStamp;
+    //            DWORD PointerToSymbolTable;
+    //            DWORD NumberOfSymbols;
+    //            WORD  SizeOfOptionalHeader;
+    //            WORD  Characteristics;
     DWORD FileSize, PeHeaderAddress, Signature;
     HANDLE fileHandle;
     HANDLE maphandle;
     LPVOID lpBase;
 
     LPCWSTR fileAddress = L"C:/Users/Karuu/Desktop/ConsoleApplication.exe";
-
     fileHandle = CreateFile(fileAddress, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
     if (fileHandle == INVALID_HANDLE_VALUE) {
         cout << "Hata :" << GetLastError() << endl;
@@ -110,8 +132,15 @@ int main()
     }
     cout << "Hata Yok!" << endl;
     dosHeader = (PIMAGE_DOS_HEADER)lpBase;
+    if (dosHeader->e_magic == IMAGE_DOS_SIGNATURE) {
+        printf("Dos exe File");
+    }
+    ntHeaders = (PIMAGE_NT_HEADERS)((BYTE*)dosHeader + (dosHeader->e_lfanew));
+    cout << ntHeaders->Signature;
+    if (ntHeaders->Signature == IMAGE_NT_SIGNATURE) {
+        printf("PE File");
+    }
 
-    cout << dosHeader->e_lfanew;
     CloseHandle(fileHandle);
     return 0;
 }
